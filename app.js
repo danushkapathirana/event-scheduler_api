c2eb78bd3982af78c93d365b112c60ba394cacab
Danushka Pathirana <119834779+danushkapathirana@users.noreply.github.com>
2024-05-26 21:59:41 +0530

    complete event-scheduler_api application

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 const express = require("express")
 require("dotenv").config()
+const bodyParser = require("body-parser")
+
+const routes = require("./routes/event")
 
 const app = express()
+app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE")
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type")
+    next()
+})
+
+app.use("/events", routes)
+
+app.use((error, req, res, next) => {
+    const status = error.status || 500
+    const message = error.message || "Something went wrong."
+    res.status(status).json({message: message})
+})
 
 app.listen(process.env.PORT || 9000, () => {
     console.log("server started")
 })
+
+
+/**
+ * libraries
+ * 
+ * npm install uuid
+ */
---
+++ data/event.js
@@ -0,0 +1,69 @@
+const fs = require("node:fs/promises")
+const { v4: generatedId } = require("uuid")
+
+const { NotFoundError } = require("../utils/error")
+
+async function readData() {
+    const data = await fs.readFile("events.json", "utf8")
+    return JSON.parse(data)
+}
+
+async function writeData(data) {
+    await fs.writeFile("events.json", JSON.stringify(data))
+}
+
+async function getAll() {
+    const storedData = await readData()
+    if(!storedData.events) {
+        throw new NotFoundError("Could not find any events.")
+    }
+
+    return storedData.events
+}
+
+async function get(id) {
+    const storedData = await readData()
+    if(!storedData.events || storedData.events.length === 0) {
+        throw new NotFoundError("Could not find any events.")
+    }
+
+    const event = storedData.events.find((event) => event.id === id)
+    if(!event) {
+        throw new NotFoundError("Could not find event for id " +id)
+    }
+
+    return event
+}
+
+async function add(data) {
+    const storedData = await readData()
+    storedData.events.unshift({ ...data, id: generatedId() })
+    await writeData(storedData)
+}
+
+async function replace(id, data) {
+    const storedData = await readData()
+    if(!storedData.events || storedData.events.length === 0) {
+        throw new NotFoundError("Could not find any events.")
+    }
+
+    const index = storedData.events.findIndex((event) => event.id === id)
+    if(index < 0) {
+        throw new NotFoundError("Could not find event for id " +id)
+    }
+
+    storedData.events[index] = { ...data, id }
+    await writeData(storedData)
+}
+
+async function remove(id) {
+    const storedData = await readData()
+    const updatedData = storedData.events.filter((event) => event.id !== id)
+    await writeData({events: updatedData})
+}
+
+exports.getAll = getAll
+exports.get = get
+exports.add = add
+exports.replace = replace
+exports.remove = remove
